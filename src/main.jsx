3390324e73eee712a149b16bf0e3a070bf072809
stanleywu <stanleywu90@gmail.com>
2024-12-17 22:41:08 +1300

    removed unused dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 import CssBaseline from '@mui/material/CssBaseline';
 import './index.css';
 import App from './App.jsx';
@@ -22,9 +20,7 @@ createRoot(document.getElementById('root')).render(
     <StrictMode>
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <DndProvider backend={HTML5Backend}>
                 <App />
-            </DndProvider>
         </ThemeProvider>
     </StrictMode>,
 );
